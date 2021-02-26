d1f37971a6d0498a4701c13620c871fd89027d84
Samandar <samandar149official@gmail.com>
2021-02-26 22:21:40 +0500

    upload

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import cls from './header.module.scss'
+function Header() {
+  return <div className={cls.txt}>sadasdas</div>
+}
+
+export default Header
---
+++ src/components/svg.js
@@ -0,0 +1,72 @@
+export const Phone = () => (
+  <svg
+    width='33'
+    height='33'
+    viewBox='0 0 33 33'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+  >
+    <path
+      d='M14.3912 8.10008L9.4577 2.40529C8.88895 1.74904 7.84624 1.75196 7.18562 2.41404L3.12853 6.47842C1.92103 7.68738 1.57541 9.48259 2.27395 10.922C6.44715 19.5625 13.4157 26.5408 22.0504 30.7261C23.4883 31.4247 25.2821 31.079 26.4896 29.8701L30.5846 25.7678C31.2481 25.1043 31.2496 24.0557 30.5875 23.487L24.8708 18.5797C24.2729 18.0663 23.344 18.1334 22.7446 18.7343L20.7554 20.7263C20.6536 20.8331 20.5195 20.9034 20.3738 20.9266C20.2282 20.9498 20.0789 20.9245 19.949 20.8547C16.6975 18.9823 14.0004 16.2817 12.1323 13.0278C12.0623 12.8976 12.037 12.7481 12.0602 12.6022C12.0834 12.4563 12.1538 12.3219 12.2606 12.2199L14.244 10.2351C14.8448 9.63134 14.9104 8.698 14.3912 8.09863V8.10008Z'
+      stroke='#363636'
+      stroke-width='3'
+      stroke-linecap='round'
+      stroke-linejoin='round'
+    />
+  </svg>
+)
+export const PhoneNum = () => (
+  <svg
+    width='126'
+    height='126'
+    viewBox='0 0 126 126'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+  >
+    <circle cx='63' cy='62' r='51.5' stroke='#289EAB' stroke-opacity='0.15' />
+    <circle cx='62.5' cy='62.5' r='44' stroke='#289EAB' stroke-opacity='0.44' />
+    <circle cx='62.5' cy='62.5' r='37.5' fill='#0298A9' />
+    <g filter='url(#filter0_d)'>
+      <circle cx='63' cy='63' r='41' fill='white' fill-opacity='0.1' />
+    </g>
+    <path
+      d='M72.9642 45.359L70.9332 44.5175C69.0267 43.7294 66.8536 44.5653 65.849 46.4694L63.8546 50.2632C62.9862 51.915 63.2193 53.9635 64.4354 55.3267L67.8172 59.1204C67.4179 61.1883 66.5443 63.1834 65.2003 65.106C63.9203 66.968 62.2797 68.5541 60.3757 69.7706L56.0868 67.9282C54.4597 67.2318 52.5851 67.7216 51.4352 69.1429L48.7508 72.4533C47.4117 74.1068 47.4232 76.5411 48.7788 78.1502L50.2321 79.8749C51.6784 81.5912 53.8857 82.3556 56.0249 81.8781C61.0771 80.7535 65.9844 76.5722 70.7485 69.3346C75.5195 62.0857 77.5183 55.7614 76.7424 50.3692C76.4164 48.1001 74.9802 46.1933 72.9642 45.359Z'
+      fill='white'
+    />
+    <defs>
+      <filter
+        id='filter0_d'
+        x='0'
+        y='0'
+        width='126'
+        height='126'
+        filterUnits='userSpaceOnUse'
+        color-interpolation-filters='sRGB'
+      >
+        <feFlood flood-opacity='0' result='BackgroundImageFix' />
+        <feColorMatrix
+          in='SourceAlpha'
+          type='matrix'
+          values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
+        />
+        <feOffset />
+        <feGaussianBlur stdDeviation='11' />
+        <feColorMatrix
+          type='matrix'
+          values='0 0 0 0 0.454167 0 0 0 0 0.454167 0 0 0 0 0.454167 0 0 0 0.13 0'
+        />
+        <feBlend
+          mode='normal'
+          in2='BackgroundImageFix'
+          result='effect1_dropShadow'
+        />
+        <feBlend
+          mode='normal'
+          in='SourceGraphic'
+          in2='effect1_dropShadow'
+          result='shape'
+        />
+      </filter>
+    </defs>
+  </svg>
+)
