da6454087716a506adf26b562506e68df02c92d7
Samandar <samandar149official@gmail.com>
2021-02-28 20:56:45 +0500

    SAsdas

--- src/components/reviews/ReviewSlider.jsx
+++ src/components/reviews/ReviewSlider.jsx
@@ -18,9 +18,9 @@ function ReviewSlider({ children }) {
         },
       },
       {
-        breakpoint: 500,
+        breakpoint: 576,
         settings: {
-          slidesToShow: 1,
+          slidesToShow: 1.4,
           slidesToScroll: 1,
           infinite: true,
           centerMode: false,
